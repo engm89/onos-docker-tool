bfd500beec6698e264e69abfda596e6f868198c7
Jian Li <pyguni@gmail.com>
2018-06-25 17:13:41 +0900

    Support test VM connectivity by pinging to VM using floatingIP

---
+++ test-conn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# SONA VM connection test with given floating IPs.
+# -----------------------------------------------------------------------------
+
+function _usage () {
+cat << _EOF_
+usage: $(basename "$0")
+
+SONA VM connection test shell script.
+
+The shell script will refer to environment variables defined in bash_profile to
+test the connctivity to VMs which assigned floating IPs.
+
+_EOF_
+}
+
+[ "$1" = "-h" ] || [ "$1" = '-?' ] && _usage && exit 0
+
+# shellcheck disable=SC1091
+source envSetup
+
+ipList=`curl -s --user onos:rocks -X GET http://$OC1:8181/onos/openstacknetworking/management/floatingips/mapped`
+python asset/ping-test $ipList
